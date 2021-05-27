c63b2bd6e039ad47aa6e03516cb5e3af63ef56c2
Iván Coello Cortés <ivancoellocortes@gmail.com>
2021-05-27 19:01:51 +0200

    Creación de las ventanas login y register
    Se han creado las ventanas de login y register con su navegación de una a la otra

---
+++ Cliente_MapaCovid/src/objetos/Escritor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ *
+ * @author ivanc
+ */
+public class Escritor {
+    
+    private DataInputStream dis;
+    private DataOutputStream dos;
+    private ObjectInputStream ois;
+    private ObjectOutputStream oos;
+    
+}
